eb419ce805ff8bb43afdebbff92fdf1fbb467497
Hadeer Khaled <Hadeer_khaled@ETAX.COM.EG>
2024-07-28 12:14:26 +0300

    create hasMany relation between product and category

--- app/Models/Category.php
+++ app/Models/Category.php
@@ -5,7 +5,15 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
+use App\Models\Product;
+
 class Category extends Model
 {
     use HasFactory;
+
+    protected $fiilable = ['title'];
+
+    public function products(): hasMany{
+        return $this->hasMany(Product::class);
+    }
 }
--- app/Models/Product.php
+++ app/Models/Product.php
@@ -5,7 +5,15 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
+use App\Models\Category;
+
 class Product extends Model
 {
     use HasFactory;
+
+    protected $fiilable = ['title' , 'description' , 'price' , 'category_id'];
+
+    public function category(): hasMany{
+        return $this->belongsTo(Category::class);
+    }
 }
