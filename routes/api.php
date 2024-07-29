b515fd9d24af92c861cdab792a8a5e5c86d93fc2
hadeer-khaled <hadeer.awad02@eng-st.cu.edu.eg>
2024-07-29 11:43:31 +0300

    product , categories , users routes

--- routes/api.php
+++ routes/api.php
@@ -4,6 +4,8 @@ use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
 use App\Http\Controllers\Api\ProductController;
+use App\Http\Controllers\Api\CategoryController;
+use App\Http\Controllers\Api\UserController;
 
 /*
 |--------------------------------------------------------------------------
@@ -26,3 +28,15 @@ Route::post('products', [ProductController::class, 'store'])->name('products.sto
 Route::get('products/{product}', [ProductController::class, 'show'])->name('products.show');
 Route::patch('products/{product}', [ProductController::class, 'update'])->name('products.update');
 Route::delete('products/{product}', [ProductController::class, 'destroy'])->name('products.destroy');
+
+Route::get('categories', [CategoryController::class, 'index'])->name('categories.index');
+Route::post('categories', [CategoryController::class, 'store'])->name('categories.store');
+Route::get('categories/{category}', [CategoryController::class, 'show'])->name('categories.show');
+Route::patch('categories/{category}', [CategoryController::class, 'update'])->name('categories.update');
+Route::delete('categories/{category}', [CategoryController::class, 'destroy'])->name('categories.destroy');
+
+Route::get('users', [UserController::class, 'index'])->name('users.index');
+Route::post('users', [UserController::class, 'store'])->name('users.store');
+Route::get('users/{user}', [UserController::class, 'show'])->name('users.show');
+Route::patch('users/{user}', [UserController::class, 'update'])->name('users.update');
+Route::delete('users/{user}', [UserController::class, 'destroy'])->name('users.destroy');
