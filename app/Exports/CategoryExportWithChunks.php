95631538f9533d32c965fb1a3eed8299731aa759
hadeer-khaled <hadeer.awad02@eng-st.cu.edu.eg>
2024-10-02 16:11:20 +0300

    style the exported excel file

--- app/Exports/CategoryExportWithChunks.php
+++ app/Exports/CategoryExportWithChunks.php
@@ -7,8 +7,12 @@ use Maatwebsite\Excel\Concerns\FromQuery;
 use Maatwebsite\Excel\Concerns\WithHeadings;
 use Maatwebsite\Excel\Concerns\WithCustomChunkSize;
 use Maatwebsite\Excel\Concerns\Exportable;
+use Maatwebsite\Excel\Concerns\WithStyles;
+use Maatwebsite\Excel\Concerns\WithEvents;
+use Maatwebsite\Excel\Events\AfterSheet;
+use PhpOffice\PhpSpreadsheet\Worksheet\Worksheet;
 
-class CategoryExportWithChunks implements FromQuery, WithHeadings, WithCustomChunkSize
+class CategoryExportWithChunks implements FromQuery, WithHeadings, WithCustomChunkSize, WithStyles
 {
     use Exportable;
 
@@ -37,9 +41,50 @@ class CategoryExportWithChunks implements FromQuery, WithHeadings, WithCustomChu
     public function headings(): array
     {
         return [
-            'ID',           
-            'Title',
+            'الرقم',           
+            'الاسم',
         ];
     }
 
+    public function styles(Worksheet $sheet)
+    {
+        $sheet->setRightToLeft(true);
+    
+        // $sheet->getStyle('A1:B1')->applyFromArray([
+        //     'font' => [
+        //         'bold' => true,
+        //     ],
+        //     'fill' => [
+        //         'fillType' => \PhpOffice\PhpSpreadsheet\Style\Fill::FILL_SOLID,
+        //         'startColor' => [
+        //             'argb' => 'FFFF0000', // Red background color (ARGB format)
+        //         ],
+        //     ],
+        // ]);
+    
+        return [  
+            1    => ['font' => ['bold' => true]],
+        ];
+    }
+    
+    
+
+    // public function registerEvents(): array
+    // {
+    //     return [
+    //         AfterSheet::class => function(AfterSheet $event) {
+    //             // Set the direction of the worksheet to RTL
+    //             $event->sheet->getDelegate()->setRightToLeft(true);
+    
+    //             // Test if the event is being triggered by setting a background color
+    //             $event->sheet->getDelegate()->getStyle('A1:B1')->applyFromArray([
+    //                 'fill' => [
+    //                     'fillType' => \PhpOffice\PhpSpreadsheet\Style\Fill::FILL_SOLID,
+    //                     'color' => ['argb' => 'FFFF0000'],
+    //                 ],
+    //             ]);
+    //         },
+    //     ];
+    // }
+    
 }
