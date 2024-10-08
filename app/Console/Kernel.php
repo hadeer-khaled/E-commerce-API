e9b78863207bae5b46e5a5d37acee81b865c71ad
hadeer-khaled <hadeer.awad02@eng-st.cu.edu.eg>
2024-10-08 10:41:17 +0300

    try task scheduling

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -4,6 +4,7 @@ namespace App\Console;
 
 use Illuminate\Console\Scheduling\Schedule;
 use Illuminate\Foundation\Console\Kernel as ConsoleKernel;
+use App\Jobs\SendExportNotification;
 
 class Kernel extends ConsoleKernel
 {
@@ -13,6 +14,14 @@ class Kernel extends ConsoleKernel
     protected function schedule(Schedule $schedule): void
     {
         // $schedule->command('inspire')->hourly();
+        $schedule->call(function () {
+            \Log::info("after 5 seconds");
+        })->everyFiveSeconds();;
+
+        
+        $schedule->job(new SendExportNotification() )->everyMinute();
+
+
     }
 
     /**
